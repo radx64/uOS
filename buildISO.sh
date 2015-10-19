2fd3f9e615365d829c036e672ea6880975b4bf31
radx64 <radek-team@wp.pl>
2015-10-19 21:57:31 +0200

    Menu for GRUB added. Building simple ISO image

---
+++ buildISO.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mkdir -p iso/boot/grub			# create folder structure
+cp build/stage2 iso/boot/grub	# copy bootloader
+cp menu.lst iso/boot/grub       # copy bootloader config
+cp build/kernel.elf iso/boot    # copy the kernel
+
+genisoimage -R					 \
+			-b boot/grub/stage2  \
+			-no-emul-boot        \
+			-boot-load-size 4    \
+			-A os                \
+			-input-charset utf8  \
+			-quiet               \
+			-boot-info-table     \
+			-o os.iso            \
+			iso
