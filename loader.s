f9661d5aada6630bb684d5d0b592a8fe12c7b608
radx64 <radek-team@wp.pl>
2015-10-20 19:25:38 +0200

    VM slowed down, kernel stack initialization

--- loader.s
+++ loader.s
@@ -5,14 +5,22 @@ FLAGS        equ 0x0            ; multiboot flags
 CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                 ; (magic number + checksum + flags should equal 0)
 
+KERNEL_STACK_SIZE equ 4096      ; size of a stack in bytes
+
 section .text:                  ; start of the text (code) section
 align 4                         ; code must be 4 byte aligned
     dd MAGIC_NUMBER             ; write the magic number to the machine code,
     dd FLAGS                    ; same with the flags
     dd CHECKSUM                 ; as well as the checksum
 
+section .bss:
+align 4                         ; align to 4 bytes
+kernel_stack:                   ; label pointing to begining of memory
+    resb KERNEL_STACK_SIZE      ; reserve stack for the kernel of size KERNEL_STACK_SIZE
+
 loader:                         ; the loader label (defined as entry point in linking script)
     mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
-
+    mov esp, kernel_stack + KERNEL_STACK_SIZE ; point esp to start of the
+                                              ; stack (end of memory area)
 .loop:
     jmp .loop                   ; loop forever
