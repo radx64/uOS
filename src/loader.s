df3410a4bc06f4803559ba269228ec340077bddf
radx64 <radek-team@wp.pl>
2015-10-20 22:34:21 +0200

    Cleanup of FB fiddling code.

--- src/loader.s
+++ src/loader.s
@@ -18,17 +18,8 @@ kernel_stack:                   ; label pointing to begining of memory
     resb KERNEL_STACK_SIZE      ; reserve stack for the kernel of size KERNEL_STACK_SIZE
 
 loader:                         ; the loader label (defined as entry point in linking script)
-    mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
     mov esp, kernel_stack + KERNEL_STACK_SIZE ; point esp to start of the
                                               ; stack (end of memory area)
-    ; mov ebx, 0x0A41
-    ; mov [0x000B8000], ebx
-    ; add ebx, 0x1
-    ; mov [0x000B8002], ebx
-    ; add ebx, 0x1
-    ; mov [0x000B8004], ebx
-    ; add ebx, 0x1
-    ; mov [0x000B8006], ebx
                                              
 extern kmain                    ; declare kmain as external function
     call kmain                  ; call main function
