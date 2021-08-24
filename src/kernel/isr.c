4841aa882992ed309248d00469ac0ea53a555992
Radek Szewczyk <radekteam@gmail.com>
2021-08-24 07:22:32 +0200

    Comment fixes.

--- src/kernel/isr.c
+++ src/kernel/isr.c
@@ -1,13 +1,14 @@
-//
-// isr.c -- High level interrupt service routines and interrupt request handlers.
-//          Part of this code is modified from Bran's kernel development tutorials.
-//          Rewritten for JamesM's kernel development tutorials.
-//
+/*
+    isr.c -- High level interrupt service routines and interrupt request handlers.
+    Part of this code is modified from Bran's kernel development tutorials.
+    Rewritten for JamesM's kernel development tutorials.
+    Ported to uOS by radx64.
+*/
 
 #include "isr.h"
 #include "drivers/serial.h"
 
-// This gets called from our ASM interrupt handler stub.
+/* This gets called from our ASM interrupt handler stub. */
 void isr_handler(registers_t regs)
 {
     (void) regs;
