1ede43a672bb5ae85f2b5c6a1aa30ef49cb47d34
radx64 <radekteam@gmail.com>
2021-09-01 22:30:41 +0200

    Some CMakes cleanup. One sublevel Cmake splitted to per module ones.

--- src/kernel/idt.c
+++ src/kernel/idt.c
@@ -6,7 +6,7 @@
 #include "drivers/serial.h"
 
 #define KERNEL_CODE_SEGMENT_OFFSET 0x08
-#define INTERRUPT_GATE 0x8e
+#define INTERRUPT_GATE 0x8E
 #define IDT_SIZE 256
 
 #include "idt.h"
@@ -36,44 +36,42 @@ void idt_set_gate(uint8_t index, uint32_t base, uint16_t selector, uint8_t flags
 void idt_init(void* keyboard_handler)
 {
     serial_write("idt_init....");
-    unsigned long keyboard_address;
-    keyboard_address = (unsigned long) keyboard_handler;
-
     /* populate IDT entry for interrupts 0-31 */
-    idt_set_gate( 0, (uint32_t) isr0 , 0x08, 0x8E);
-    idt_set_gate( 1, (uint32_t) isr1 , 0x08, 0x8E);
-    idt_set_gate( 2, (uint32_t) isr2 , 0x08, 0x8E);
-    idt_set_gate( 3, (uint32_t) isr3 , 0x08, 0x8E);
-    idt_set_gate( 4, (uint32_t) isr4 , 0x08, 0x8E);
-    idt_set_gate( 5, (uint32_t) isr5 , 0x08, 0x8E);
-    idt_set_gate( 6, (uint32_t) isr6 , 0x08, 0x8E);
-    idt_set_gate( 7, (uint32_t) isr7 , 0x08, 0x8E);
-    idt_set_gate( 8, (uint32_t) isr8 , 0x08, 0x8E);
-    idt_set_gate( 9, (uint32_t) isr9 , 0x08, 0x8E);
-    idt_set_gate( 10, (uint32_t) isr10 , 0x08, 0x8E);
-    idt_set_gate( 11, (uint32_t) isr11 , 0x08, 0x8E);
-    idt_set_gate( 12, (uint32_t) isr12 , 0x08, 0x8E);
-    idt_set_gate( 13, (uint32_t) isr13 , 0x08, 0x8E);
-    idt_set_gate( 14, (uint32_t) isr14 , 0x08, 0x8E);
-    idt_set_gate( 15, (uint32_t) isr15 , 0x08, 0x8E);
-    idt_set_gate( 16, (uint32_t) isr16 , 0x08, 0x8E);
-    idt_set_gate( 17, (uint32_t) isr17 , 0x08, 0x8E);
-    idt_set_gate( 18, (uint32_t) isr18 , 0x08, 0x8E);
-    idt_set_gate( 19, (uint32_t) isr19 , 0x08, 0x8E);
-    idt_set_gate( 20, (uint32_t) isr20 , 0x08, 0x8E);
-    idt_set_gate( 21, (uint32_t) isr21 , 0x08, 0x8E);
-    idt_set_gate( 22, (uint32_t) isr22 , 0x08, 0x8E);
-    idt_set_gate( 23, (uint32_t) isr23 , 0x08, 0x8E);
-    idt_set_gate( 24, (uint32_t) isr24 , 0x08, 0x8E);
-    idt_set_gate( 25, (uint32_t) isr25 , 0x08, 0x8E);
-    idt_set_gate( 26, (uint32_t) isr26 , 0x08, 0x8E);
-    idt_set_gate( 27, (uint32_t) isr27 , 0x08, 0x8E);
-    idt_set_gate( 28, (uint32_t) isr28 , 0x08, 0x8E);
-    idt_set_gate( 29, (uint32_t) isr29 , 0x08, 0x8E);
-    idt_set_gate( 30, (uint32_t) isr30 , 0x08, 0x8E);
-    idt_set_gate( 31, (uint32_t) isr31 , 0x08, 0x8E);
+    idt_set_gate(0, (uint32_t) isr0,   KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(1, (uint32_t) isr1,   KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(2, (uint32_t) isr2,   KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(3, (uint32_t) isr3,   KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(4, (uint32_t) isr4,   KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(5, (uint32_t) isr5,   KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(6, (uint32_t) isr6,   KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(7, (uint32_t) isr7,   KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(8, (uint32_t) isr8,   KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(9, (uint32_t) isr9,   KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(10, (uint32_t) isr10, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(11, (uint32_t) isr11, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(12, (uint32_t) isr12, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(13, (uint32_t) isr13, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(14, (uint32_t) isr14, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(15, (uint32_t) isr15, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(16, (uint32_t) isr16, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(17, (uint32_t) isr17, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(18, (uint32_t) isr18, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(19, (uint32_t) isr19, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(20, (uint32_t) isr20, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(21, (uint32_t) isr21, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(22, (uint32_t) isr22, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(23, (uint32_t) isr23, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(24, (uint32_t) isr24, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(25, (uint32_t) isr25, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(26, (uint32_t) isr26, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(27, (uint32_t) isr27, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(28, (uint32_t) isr28, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(29, (uint32_t) isr29, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(30, (uint32_t) isr30, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
+    idt_set_gate(31, (uint32_t) isr31, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
 
     /* populate IDT entry of keyboard's interrupt */
+    unsigned long keyboard_address = (unsigned long) keyboard_handler;
     idt_set_gate(33, keyboard_address, KERNEL_CODE_SEGMENT_OFFSET, INTERRUPT_GATE);
 
     /*     Ports
@@ -104,8 +102,8 @@ void idt_init(void* keyboard_handler)
     /* Initialization finished */
 
     /* mask interrupts */
-    outb(0x21 , 0xff);
-    outb(0xA1 , 0xff);
+    outb(0x21 , 0xFF);
+    outb(0xA1 , 0xFF);
 
     /* fill the IDT descriptor */
     unsigned long idt_address;
